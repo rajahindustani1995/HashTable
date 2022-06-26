e5fd444f4be848994d46aeae2f6ff0ddaa02c316
rajahindustani1995 <rajeshdambavale959@gmail.com>
2022-06-26 21:08:41 +0530

    [Rajesh] Added : Find Frequency Of Words

---
+++ HashTable/HashTable/MyMapNode.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTable
+{
+    internal class MyMapNode<K,V>
+    {
+        internal int size;
+
+        //declarin an array of LinkedList of KeyValue pair containing K, V as key and value of name items
+        internal LinkedList<KeyValue<K, V>>[] items;
+
+        public MyMapNode(int size)
+        {
+            this.size = size;
+
+            //size of array of LinkedList is passed while creating objects in main program
+            items = new LinkedList<KeyValue<K, V>>[size];
+        }
+
+        //to get the position of the key in the array by formula given
+        public int ArrayPosition(K key)
+        {
+            //to resolve conflicts in array , we use formula to calculate position
+            //position = (hash code of the key) % (size of the array)
+            int position = key.GetHashCode() % size;
+
+            //returning the positive value of position
+            return Math.Abs(position);
+        }
+
+        public V Get(K key)
+        {
+            int position = ArrayPosition(key);
+            LinkedList<KeyValue<K, V>> linkedlist = GetLinkedList(position);
+
+            //checking for each KeyValue pair in our linked list
+            foreach (KeyValue<K, V> item in linkedlist)
+            {
+                //if our Key in item is matching with the key or not
+                if (item.Key.Equals(key))
+                {
+                    //return the value present in item , i.e. value situated at the position
+                    return item.Value;
+                }
+            }
+            return default(V);
+        }
+
+        //For adding items in our LinkedList
+        public void Add(K key, V value)
+        {
+            //get position of our item using key
+            int position = ArrayPosition(key);
+
+            LinkedList<KeyValue<K, V>> linkedlist = GetLinkedList(position);
+            KeyValue<K, V> item = new KeyValue<K, V>() { Key = key, Value = value };
+
+            //add that item at the last position/ at the end of the linkedlist
+            linkedlist.AddLast(item);
+        }
+
+        //creatig GetLinkedList method having return type as LinkedList
+        //each value in LinkedList is containing KeyValue structure
+        public LinkedList<KeyValue<K, V>> GetLinkedList(int position)
+        {
+            //passing the position of the item to the LinkedList named linkedlist
+            LinkedList<KeyValue<K, V>> linkedlist = items[position];
+
+            if (linkedlist == null)
+            {
+                linkedlist = new LinkedList<KeyValue<K, V>>();
+                items[position] = linkedlist;
+            }
+            return linkedlist;
+        }
+
+        
+        public struct KeyValue<k, v>
+        {
+            public k Key { get; set; }
+            public v Value { get; set; }
+        }
+    }
+}
---
+++ HashTable/HashTable/Program.cs
@@ -0,0 +1,70 @@
+﻿namespace HashTable
+{
+    class Program
+    {
+        public static void Main(String [] args)
+        {
+           
+            string frequency = "Fear leads to anger ; anger leads to hatred ; hatred leads to conflict ; conflict leads to suffering.";
+
+            //declaring List to compare hash table items with
+            List<string> compareList = new List<string>();
+
+            //declaring List to store word and frequency
+            List<string> storeCountList = new List<string>();
+
+            //spliting the string and converting it to an array 
+            string[] frequencyArray = frequency.Split(' ');
+
+            //decclaring hash table of the length of frequencyArray
+            MyMapNode<int, string> hash1 = new MyMapNode<int, string>(frequencyArray.Length);
+            for (int i = 0; i < frequencyArray.Length; i++)
+            {
+                //Adding each word to hash table
+                hash1.Add(i, frequencyArray[i]);
+
+                //Displaying each word stored in the hash table
+                Console.Write(hash1.Get(i) + " ");
+            }
+            Console.WriteLine();
+            for (int i = 0; i < frequencyArray.Length; i++)
+            {
+                //declaring count to count number of occurances of the words
+                int count = 0;
+                for (int j = 0; j < frequencyArray.Length; j++)
+                {
+                    //if our phrase array contains same word
+                    if (frequencyArray[j].ToLower() == hash1.Get(i).ToLower())
+                    {
+                        //then we will increase the count
+                        count++;
+
+                        
+                        if (compareList.Contains(hash1.Get(j).ToLower()))
+                        {
+                            //if value is already available in our compareList2 list then we'll break internal loop
+                            break;
+                        }
+                    }
+                }
+                
+                if (compareList.Contains(hash1.Get(i).ToLower()))
+                {
+                    continue;
+                }
+                //Adding the word to compareList for further comparison for duplicates
+                compareList.Add(hash1.Get(i));
+
+                //finally storing the word and its frequency in storeCountList list
+                storeCountList.Add(hash1.Get(i) + "\t" + count);
+            }
+
+            
+            Console.WriteLine("Words and their frequencies shown below");
+            for (int i = 0; i < storeCountList.Count; i++)
+            {
+                Console.WriteLine(storeCountList[i]);
+            }
+        }
+    }
+}
